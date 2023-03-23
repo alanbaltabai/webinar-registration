752dcc95a33d4afa91e54a8953aed348dbe32802
Alan Baltabay <baltabay123@gmail.com>
2023-03-23 19:03:50 +0600

    feat: add outlines to clickable elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react';
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import Toggler from './components/Toggler';
 
 export default function App() {
+	const body = document.body;
+	const [isDark, setIsDark] = useState(false);
+
+	function toggle(): void {
+		setIsDark((prev) => !prev);
+	}
+
+	useEffect(() => {
+		body.classList.toggle('dark');
+	}, [isDark]);
+
 	return (
 		<>
-			<Toggler />
+			<Toggler toggle={toggle} />
 			<Header />
 			<Main />
 			<Footer />
