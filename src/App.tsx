3a213eecfabdb493fb514470b9ba90acc6fa508b
Alan Baltabay <baltabay123@gmail.com>
2023-03-23 11:14:03 +0600

    feat: add dark/light theme icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer';
+import Toggler from './components/Toggler';
 
 export default function App() {
 	return (
 		<>
+			<Toggler />
 			<Header />
 			<Main />
 			<Footer />
